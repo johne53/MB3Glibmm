a63fd0f32e0e8c5db8a4810791d60ed57813069b
John Emmas <johne53@tiscali.co.uk>
2014-03-24 13:50:18 +0000

    JOHNE - Bump the version number to 2.39.92

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -3,11 +3,11 @@
 
 require "../local-paths.lib";
 
-$glibmm_version = "2.39.91";
+$glibmm_version = "2.39.92";
 $major = 2;
 $minor = 39;
-$micro = 91;
-$binary_age = 3991;
+$micro = 92;
+$binary_age = 3992;
 $interface_age = 1;
 $current_minus_age = 0;
 $exec_prefix = "lib";
